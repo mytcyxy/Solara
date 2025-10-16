35c59e1ed03f3767575f19ab7114f5ef7e461913
pite <35644249+mytcyxy@users.noreply.github.com>
2025-10-16 18:33:12 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara1";
+        dom.mobileToolbarTitle.textContent = "Solara11111111";
     }
 
     function updateMobileOverlayScrim() {
